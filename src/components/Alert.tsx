a16dde149d1eb6774decf2485e2fb6132ae1ec2d
Josh Kuttler <yoshuakuttler@gmail.com>
2019-10-23 18:48:13 +0300

    add Link component and update Alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,14 @@
 import React, { Component, CSSProperties } from 'react';
+import Link from './Link';
 
 type AlertProps = {
     text: string,
-    bgColor?: string
+    bgColor?: string,
+    link?: string
 }
 
 const css: CSSProperties = {
+    display: 'inline-block',
     position: 'relative',
     padding: '0.75rem 1.25rem',
     border: '1px solid transparent',
@@ -17,7 +20,16 @@ const css: CSSProperties = {
 
 export default class Alert extends Component<AlertProps, {}> {
     render() {
-        const { text, bgColor = 'green' } = this.props;
+        const { text, bgColor = 'green', link } = this.props;
+        if (link) {
+            return (
+                <Link link={link}>
+                    <div style={{ backgroundColor: bgColor, ...css }}>
+                        {text}
+                    </div>
+                </Link>
+            )
+        }
         return (
             <div style={{ backgroundColor: bgColor, ...css }}>
                 {text}
---
+++ src/components/Link.tsx
@@ -0,0 +1,17 @@
+import React, { Component, CSSProperties } from 'react';
+
+type LinkProps = {
+    link: string
+}
+
+const css: CSSProperties = {
+    fontSize: 16,
+    color: '#000000',
+}
+
+export default class Link extends Component<LinkProps, {}> {
+    render() {
+        const { link, children } = this.props;
+        return <a href={link} style={css}>{children}</a>
+    }
+}
